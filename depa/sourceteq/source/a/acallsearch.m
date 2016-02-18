6502bb38878baa62f598b8d58cd289b184c0cea8
vauxhall <zero.griffin@gmail.com>
2016-02-18 12:39:55 -0600

    model

--- depa/sourceteq/source/a/acallsearch.m
+++ depa/sourceteq/source/a/acallsearch.m
@@ -21,6 +21,4 @@
     self.endpoint = [NSString stringWithFormat:rawendpoint, countryid];
 }
 
-//https://api.mercadolibre.com/sites/MLM/search?has_pictures=yes&limit=1&offset=0&category=1479
-
 @end
