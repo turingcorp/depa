76e4923f39faa5df9470f546f80cc7b5267dae6e
vauxhall <zero.griffin@gmail.com>
2016-02-18 10:31:51 -0600

    acall

--- depa/sourceteq/source/a/acall.h
+++ depa/sourceteq/source/a/acall.h
@@ -2,9 +2,13 @@
 
 @interface acall:NSObject
 
+-(instancetype)init:(NSString*)variables;
+-(NSURLRequest*)request;
+
 @property(copy, nonatomic)NSString *server;
 @property(copy, nonatomic)NSString *endpoint;
 @property(copy, nonatomic)NSString *variables;
 @property(nonatomic)NSUInteger timeout;
+@property(nonatomic)BOOL post;
 
 @end
--- depa/sourceteq/source/a/acall.m
+++ depa/sourceteq/source/a/acall.m
@@ -2,4 +2,33 @@
 
 @implementation acall
 
+-(instancetype)init:(NSString*)variables
+{
+    self = [super init];
+    
+    self.variables = variables;
+    self.post = NO;
+    
+    return self;
+}
+
+#pragma mark public
+
+-(NSURLRequest*)request
+{
+    NSMutableURLRequest *request;
+    NSMutableString *urlstring = [NSMutableString string];
+    
+    if(self.post)
+    {
+        
+    }
+    else
+    {
+        
+    }
+    
+    return request;
+}
+
 @end
