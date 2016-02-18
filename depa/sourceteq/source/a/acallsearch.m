e5976ac39f25814db48aa2eb01a6cce249245a56
vauxhall <zero.griffin@gmail.com>
2016-02-18 12:36:35 -0600

    model

--- depa/sourceteq/source/a/acallsearch.m
+++ depa/sourceteq/source/a/acallsearch.m
@@ -11,6 +11,16 @@
     return self;
 }
 
+#pragma mark -
+#pragma mark acall
+
+-(void)buildendpoint:(NSDictionary*)params
+{
+    NSString *countryid = @"MLM";
+    NSString *rawendpoint = params[@"search"];
+    self.endpoint = [NSString stringWithFormat:rawendpoint, countryid];
+}
+
 //https://api.mercadolibre.com/sites/MLM/search?has_pictures=yes&limit=1&offset=0&category=1479
 
 @end
