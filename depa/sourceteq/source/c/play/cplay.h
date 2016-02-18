d150fefd73b9ef346a0e38261bbb385e524a9d34
vauxhall <zero.griffin@gmail.com>
2016-02-18 08:54:43 -0600

    project setup

--- depa/sourceteq/source/c/play/cplay.h
+++ depa/sourceteq/source/c/play/cplay.h
@@ -1,13 +1,5 @@
-//
-//  cplay.h
-//  depa
-//
-//  Created by zero on 2/18/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
+#import "appdel.h"
 
-#import <Foundation/Foundation.h>
-
-@interface cplay : NSObject
+@interface cplay:UIViewController
 
 @end
--- depa/sourceteq/source/c/play/cplay.m
+++ depa/sourceteq/source/c/play/cplay.m
@@ -1,11 +1,3 @@
-//
-//  cplay.m
-//  depa
-//
-//  Created by zero on 2/18/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "cplay.h"
 
 @implementation cplay
