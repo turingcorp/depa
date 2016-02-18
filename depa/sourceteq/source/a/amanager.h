1885bb0c96a80f95b2e09d48174337602f25061d
vauxhall <zero.griffin@gmail.com>
2016-02-18 10:24:31 -0600

    a call delegate

--- depa/sourceteq/source/a/amanager.h
+++ depa/sourceteq/source/a/amanager.h
@@ -6,7 +6,6 @@
 @interface amanager:NSObject<NSURLSessionDelegate, NSURLSessionTaskDelegate, NSURLSessionDataDelegate>
 
 +(void)call:(apicall)call delegate:(id<acalldelegate>)delegate valriables:(NSString*)variables;
--(void)cancel;
 
 @property(weak, nonatomic)id<acalldelegate> delegate;
 @property(strong, nonatomic)id response;
--- depa/sourceteq/source/a/amanager.m
+++ depa/sourceteq/source/a/amanager.m
@@ -1,13 +1,24 @@
-//
-//  amanager.m
-//  depa
-//
-//  Created by zero on 2/18/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "amanager.h"
 
 @implementation amanager
+
++(void)call:(apicall)call delegate:(id<acalldelegate>)delegate valriables:(NSString*)variables
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0),
+                   ^
+                   {
+                       
+                   });
+}
+
+-(instancetype)init:(acall*)call delegate:(id<acalldelegate>)delegate
+{
+    self = [super init];
+    
+    self.call = call;
+    self.delegate = delegate;
+    
+    return self;
+}
 
 @end
