44e0c1f0b89190130ee65f3e8848c85ffa6d44b1
vauxhall <zero.griffin@gmail.com>
2016-02-18 13:13:20 -0600

    model

--- depa/sourceteq/source/a/parser/aparser.m
+++ depa/sourceteq/source/a/parser/aparser.m
@@ -6,7 +6,34 @@
 {
     self = [super init];
     
+    NSDictionary *validjson = [self basicparse:json];
+    
     return self;
+}
+
+#pragma mark functionality
+
+-(id)basicparse:(id)object
+{
+    id validobject;
+    
+    if(object)
+    {
+        if([object isKindOfClass:[NSDictionary class]])
+        {
+            validobject = [NSMutableDictionary  dictionary];
+        }
+        else if([object isKindOfClass:[NSArray class]])
+        {
+            validobject = [NSMutableArray array];
+        }
+        else
+        {
+            validobject = object;
+        }
+    }
+    
+    return validobject;
 }
 
 @end
