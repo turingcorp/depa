441f5a4af8edf03a07238404adcdd92a2da5be24
vauxhall <zero.griffin@gmail.com>
2016-02-18 16:02:38 -0600

    controller

--- depa/sourceteq/source/c/play/cplay.m
+++ depa/sourceteq/source/c/play/cplay.m
@@ -20,4 +20,17 @@
     return NO;
 }
 
+#pragma mark functionality
+
+-(void)changecontroller:(UIViewController*)controller direction:(UIPageViewControllerNavigationDirection)direction animated:(BOOL)animated
+{
+    NSArray *array = @[controller];
+    [self setViewControllers:array direction:direction animated:animated completion:nil];
+}
+
+-(void)showloading:(BOOL)animated
+{
+    [self changecontroller:[[cplayload alloc] init] direction:UIPageViewControllerNavigationDirectionForward animated:animated];
+}
+
 @end
