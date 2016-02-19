7040e5985e95f18e5f4a2158641b494fcdb74664
vauxhall <zero.griffin@gmail.com>
2016-02-18 18:15:51 -0600

    collection

--- depa/sourceteq/source/v/play/vplaymenu.m
+++ depa/sourceteq/source/v/play/vplaymenu.m
@@ -2,4 +2,27 @@
 
 @implementation vplaymenu
 
+-(instancetype)init
+{
+    self = [super init];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
+    [collection setBackgroundColor:[UIColor clearColor]];
+    [collection setClipsToBounds:YES];
+    [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [collection setDelegate:self];
+    [collection setDataSource:self];
+    [collection setScrollEnabled:NO];
+    [collection setBounces:NO];
+    [collection setBackgroundColor:[UIColor clearColor]];
+    self.collection = collection;
+    
+    return self;
+}
+
 @end
---
+++ depa/sourceteq/source/v/play/vplaymenucel.m
@@ -0,0 +1,14 @@
+#import "vplaymenucel.h"
+
+@implementation vplaymenucel
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    
+    return self;
+}
+
+@end
