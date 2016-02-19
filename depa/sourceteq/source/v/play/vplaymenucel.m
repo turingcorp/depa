9b12feb99aa87835ea5116b0244613f77841fb4a
vauxhall <zero.griffin@gmail.com>
2016-02-19 16:18:22 -0600

    assets size

--- depa/sourceteq/source/v/play/vplaymenucel.m
+++ depa/sourceteq/source/v/play/vplaymenucel.m
@@ -21,7 +21,7 @@
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[image]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-25-[image]-25-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-5-[image]-5-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
