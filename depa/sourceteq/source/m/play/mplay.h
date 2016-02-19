5741d206d4e817a5d5ed22c9ac97c57b752ac0cd
vauxhall <zero.griffin@gmail.com>
2016-02-18 18:17:18 -0600

    model

---
+++ depa/sourceteq/source/m/play/mplay.h
@@ -0,0 +1,13 @@
+//
+//  mplay.h
+//  depa
+//
+//  Created by zero on 2/18/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mplay : NSObject
+
+@end
---
+++ depa/sourceteq/source/m/play/mplay.m
@@ -0,0 +1,13 @@
+//
+//  mplay.m
+//  depa
+//
+//  Created by zero on 2/18/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mplay.h"
+
+@implementation mplay
+
+@end
