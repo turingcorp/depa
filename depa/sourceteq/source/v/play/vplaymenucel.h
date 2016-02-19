d4ab9d133a03e99bad063420bc106a8b2552d095
vauxhall <zero.griffin@gmail.com>
2016-02-19 12:31:32 -0600

    blur

--- depa/sourceteq/source/v/play/vplaymenucel.h
+++ depa/sourceteq/source/v/play/vplaymenucel.h
@@ -1,9 +1,12 @@
 #import "appdel.h"
 
+@class vblur;
+
 @interface vplaymenucel:UICollectionViewCell
 
 -(void)config:(id<mplaymenuprotocol>)model;
 
+@property(weak, nonatomic)vblur *blur;
 @property(weak, nonatomic)UIImageView *image;
 
 @end
--- depa/sourceteq/source/v/play/vplaymenucel.m
+++ depa/sourceteq/source/v/play/vplaymenucel.m
@@ -8,7 +8,8 @@
     [self setClipsToBounds:YES];
     [self setBackgroundColor:[UIColor clearColor]];
 
-    vblur *blur;
+    vblur *blur = [vblur light:YES];
+    self.blur = blur;
     
     UIImageView *image = [[UIImageView alloc] init];
     [image setContentMode:UIViewContentModeScaleAspectFit];
@@ -17,20 +18,39 @@
     [image setTranslatesAutoresizingMaskIntoConstraints:NO];
     self.image = image;
     
-    NSDictionary *views = @{};
+    [self addSubview:blur];
+    [self addSubview:image];
+    
+    NSDictionary *views = @{@"image":image, @"blur":blur};
     NSDictionary *metrics = @{};
     
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[image]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[image]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[image]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-20-[image]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[blur]-10-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-10-[blur]-10-|" options:0 metrics:metrics views:views]];
     
     return self;
+}
+
+-(void)layoutSubviews
+{
+    CGFloat blurwidth = self.blur.bounds.size.width;
+    CGFloat radius = blurwidth / 2.0;
+    
+    dispatch_async(dispatch_get_main_queue(),
+                   ^
+                   {
+                       [self.blur.layer setCornerRadius:radius];
+                   });
+    
+    [super layoutSubviews];
 }
 
 #pragma mark public
 
 -(void)config:(id<mplaymenuprotocol>)model
 {
-    [self.image setImage:[model assetname]];
+    [self.image setImage:[UIImage imageNamed:[model assetname]]];
 }
 
 @end
