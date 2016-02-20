9957261a930baa03a9706110701124f1b1653eda
vauxhall <zero.griffin@gmail.com>
2016-02-20 15:32:23 -0600

    empty

--- depa/sourceteq/source/v/play/vplayempty.m
+++ depa/sourceteq/source/v/play/vplayempty.m
@@ -12,10 +12,11 @@
     [label setBackgroundColor:[UIColor clearColor]];
     [label setUserInteractionEnabled:NO];
     [label setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [label setFont:[UIFont fontWithName:fontname size:18]];
-    [label setTextColor:[UIColor colorWithWhite:0 alpha:0.7]];
+    [label setFont:[UIFont fontWithName:fontname size:20]];
+    [label setTextColor:[UIColor colorWithWhite:0 alpha:0.5]];
     [label setText:NSLocalizedString(@"play_empty", nil)];
     [label setTextAlignment:NSTextAlignmentCenter];
+    [label setNumberOfLines:0];
     
     UIButton *button = [[UIButton alloc] init];
     [button setTranslatesAutoresizingMaskIntoConstraints:NO];
@@ -24,7 +25,7 @@
     [button setTitleColor:[UIColor colorWithWhite:1 alpha:0.2] forState:UIControlStateHighlighted];
     [button setBackgroundColor:colormain];
     [button setTitle:NSLocalizedString(@"play_empty_button", nil) forState:UIControlStateNormal];
-    [button.titleLabel setFont:[UIFont fontWithName:fontboldname size:14]];
+    [button.titleLabel setFont:[UIFont fontWithName:fontboldname size:16]];
     [button.layer setCornerRadius:4];
     
     [self addSubview:label];
@@ -33,9 +34,9 @@
     NSDictionary *views = @{@"label":label, @"button":button};
     NSDictionary *metrics = @{};
     
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[label]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-25-[label]-25-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-80-[button]-80-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-150-[label]-10-[button(40)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-150-[label]-20-[button(40)]" options:0 metrics:metrics views:views]];
     
     return self;
 }
