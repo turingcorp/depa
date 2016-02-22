11d972610cdea0c5e0f5a56a72b91fc3d3434510
vauxhall <zero.griffin@gmail.com>
2016-02-22 11:42:38 -0600

    controller

--- depa/sourceteq/source/v/config/vconfig.m
+++ depa/sourceteq/source/v/config/vconfig.m
@@ -6,7 +6,7 @@
 {
     self = [super init:controller];
     [self setClipsToBounds:YES];
-    [self setBackgroundColor:[UIColor redColor]];
+    [self setBackgroundColor:[UIColor colorWithWhite:0.97 alpha:1]];
     
     return self;
 }
