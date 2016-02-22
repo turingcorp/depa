f37e499b90d68b9435fa416f7f55a7e97dc088f3
vauxhall <zero.griffin@gmail.com>
2016-02-22 10:31:05 -0600

    animations

--- depa/sourceteq/source/m/play/mplaymenuno.m
+++ depa/sourceteq/source/m/play/mplaymenuno.m
@@ -21,7 +21,55 @@
 
 -(void)action:(cplayitem*)controller
 {
-    [controller.play playno];
+    vplay *view = (vplay*)controller.view;
+    vplayitemimage *itemimage = view.item.image;
+    
+    UIImageView *image = [[UIImageView alloc] init];
+    [image setClipsToBounds:YES];
+    [image setContentMode:UIViewContentModeScaleAspectFit];
+    [image setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [image setUserInteractionEnabled:NO];
+    [image setImage:[[UIImage imageNamed:[self assetname]] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
+    [image setTintColor:[self tintcolor]];
+    [image setAlpha:0];
+    
+    [itemimage addSubview:image];
+    
+    CGFloat imageheight = 140;
+    CGFloat itemheight = itemimage.bounds.size.height;
+    CGFloat imagemargin = (itemheight - imageheight) / 2.0;
+    
+    NSDictionary *views = @{@"image":image};
+    NSDictionary *metrics = @{@"height":@(imageheight), @"margin":@(imagemargin)};
+    
+    NSLayoutConstraint *constr = [NSLayoutConstraint constraintWithItem:image attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:itemimage attribute:NSLayoutAttributeTop multiplier:1 constant:-imagemargin];
+    
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[image]-0-|" options:0 metrics:metrics views:views]];
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[image(height)]" options:0 metrics:metrics views:views]];
+    [view addConstraint:constr];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_MSEC * 10), dispatch_get_main_queue(),
+                   ^
+                   {
+                       constr.constant = imagemargin;
+                       
+                       [UIView animateWithDuration:0.3 animations:
+                        ^
+                        {
+                            [image setAlpha:1];
+                            [itemimage layoutIfNeeded];
+                        } completion:
+                        ^(BOOL done)
+                        {
+                            [controller.play playno];
+                        }];
+                   });
+    
+    [UIView animateWithDuration:0.8 animations:
+     ^
+     {
+         [itemimage.image setAlpha:0];
+     }];
 }
 
 @end
