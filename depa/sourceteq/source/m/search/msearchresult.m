5bbc7521b5f975541bee84e4532907bb273692a7
vauxhall <zero.griffin@gmail.com>
2016-02-22 10:42:32 -0600

    item title case

--- depa/sourceteq/source/m/search/msearchresult.m
+++ depa/sourceteq/source/m/search/msearchresult.m
@@ -7,8 +7,18 @@
     self = [super init];
 
     self.itemid = rawitem[@"id"];
-    self.itemtitle = rawitem[@"title"];
     self.itemprice = rawitem[@"price"];
+    
+    NSString *itemrawtitle = rawitem[@"title"];
+    
+    if(itemrawtitle.length > 1)
+    {
+        NSString *titleprefix = [itemrawtitle substringToIndex:1].uppercaseString;
+        NSString *titlesuffix = [itemrawtitle substringFromIndex:1].lowercaseString;
+        itemrawtitle = [NSString stringWithFormat:@"%@%@", titleprefix, titlesuffix];
+    }
+    
+    self.itemtitle = itemrawtitle;
     
     NSString *thumbnail = rawitem[@"thumbnail"];
     thumbnail = [thumbnail stringByReplacingOccurrencesOfString:@"-I.jpg" withString:@"-O.jpg"];
