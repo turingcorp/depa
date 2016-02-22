40430012a019c0456c51ca88381e9f3cda86985a
vauxhall <zero.griffin@gmail.com>
2016-02-22 17:22:22 -0600

    collection

--- depa/sourceteq/source/v/configloc/vconfigloc.m
+++ depa/sourceteq/source/v/configloc/vconfigloc.m
@@ -1,6 +1,9 @@
 #import "vconfigloc.h"
 
 @implementation vconfigloc
+{
+    NSUInteger menuheight;
+}
 
 -(instancetype)init:(cconfigloc*)controller
 {
@@ -8,7 +11,14 @@
     [self setClipsToBounds:YES];
     [self setBackgroundColor:[UIColor whiteColor]];
 
+    menuheight = 80;
+    
     UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    [flow setFooterReferenceSize:CGSizeZero];
+    [flow setMinimumInteritemSpacing:0];
+    [flow setMinimumLineSpacing:0];
+    [flow setScrollDirection:UICollectionViewScrollDirectionVertical];
+    [flow setSectionInset:UIEdgeInsetsZero];
     
     UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
     [collection setClipsToBounds:YES];
@@ -17,11 +27,47 @@
     [collection setDelegate:self];
     [collection setShowsHorizontalScrollIndicator:NO];
     [collection setShowsVerticalScrollIndicator:NO];
-    [collection registerClass:[] forCellWithReuseIdentifier:<#(nonnull NSString *)#>];
+    [collection setBounces:NO];
+    [collection setScrollEnabled:NO];
+    [collection registerClass:[vconfiglocmenu class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:headerid];
     
     [self addSubview:collection];
     
     return self;
+}
+
+#pragma mark -
+#pragma mark col del
+
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout referenceSizeForHeaderInSection:(NSInteger)section
+{
+    CGSize size = CGSizeMake(col.bounds.size.width, menuheight);
+    
+    return size;
+}
+
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout sizeForItemAtIndexPath:(NSIndexPath*)index
+{
+    CGSize size = CGSizeMake(col.bounds.size.width, col.bounds.size.height - menuheight);
+    
+    return size;
+}
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
+{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView*)col numberOfItemsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(UICollectionViewCell*)collectionView:(UICollectionView*)col cellForItemAtIndexPath:(NSIndexPath*)index
+{
+    UICollectionViewCell *cel = [col dequeueReusableCellWithReuseIdentifier:celid forIndexPath:index];
+    
+    return cel;
 }
 
 @end
