d3fdef4bc63ac1780662ca96dfffbb7db88a6cb1
vauxhall <zero.griffin@gmail.com>
2016-02-22 10:04:59 -0600

    controller

--- depa/sourceteq/source/m/play/mplaymenulike.m
+++ depa/sourceteq/source/m/play/mplaymenulike.m
@@ -42,14 +42,19 @@
     NSDictionary *views = @{@"image":image};
     NSDictionary *metrics = @{@"height":@(imageheight), @"margin":@(imagemargin)};
     
-    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[image]-0-|" options:0 metrics:metrics views:views]];
-    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(margin)-[image(height)]" options:0 metrics:metrics views:views]];
+    NSLayoutConstraint *constr = [NSLayoutConstraint constraintWithItem:image attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:itemimage attribute:NSLayoutAttributeBottom multiplier:1 constant:0];
     
-    [UIView animateWithDuration:0.5 animations:
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[image]-0-|" options:0 metrics:metrics views:views]];
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[image(height)]" options:0 metrics:metrics views:views]];
+    [view addConstraint:constr];
+    constr.constant = -imagemargin;
+    
+    [UIView animateWithDuration:2 animations:
      ^
      {
-         [itemimage.image setAlpha:0.1];
+         [itemimage.image setAlpha:0.3];
          [image setAlpha:1];
+         [image layoutIfNeeded];
      } completion:
      ^(BOOL done)
      {
