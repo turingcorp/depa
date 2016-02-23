25753641a6cfeff310a0da4b444ab0c93c445764
vauxhall <zero.griffin@gmail.com>
2016-02-23 11:09:58 -0600

    protocol

--- depa/sourceteq/source/m/configmod/mconfigmodbuy.m
+++ depa/sourceteq/source/m/configmod/mconfigmodbuy.m
@@ -2,4 +2,21 @@
 
 @implementation mconfigmodbuy
 
+#pragma mark -
+#pragma mark config mode protocol
+
+-(NSString*)title
+{
+    NSString *title = NSLocalizedString(@"", nil);
+    
+    return title;
+}
+
+-(NSString*)assetname
+{
+    NSString *assetname = @"";
+    
+    return assetname;
+}
+
 @end
--- depa/sourceteq/source/m/configmod/mconfigmodrent.m
+++ depa/sourceteq/source/m/configmod/mconfigmodrent.m
@@ -2,4 +2,21 @@
 
 @implementation mconfigmodrent
 
+#pragma mark -
+#pragma mark config mode protocol
+
+-(NSString*)title
+{
+    NSString *title = NSLocalizedString(@"", nil);
+    
+    return title;
+}
+
+-(NSString*)assetname
+{
+    NSString *assetname = @"";
+    
+    return assetname;
+}
+
 @end
