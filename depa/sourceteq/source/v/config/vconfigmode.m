90753a8472706e2f2ed46743f3c57b4f8181480b
vauxhall <zero.griffin@gmail.com>
2016-02-23 11:03:36 -0600

    collection

--- depa/sourceteq/source/v/config/vconfigmode.m
+++ depa/sourceteq/source/v/config/vconfigmode.m
@@ -10,6 +10,11 @@
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
     
     UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    [flow setHeaderReferenceSize:CGSizeZero];
+    [flow setFooterReferenceSize:CGSizeZero];
+    [flow setMinimumInteritemSpacing:0];
+    [flow setMinimumLineSpacing:0];
+    [flow setScrollDirection:UICollectionViewScrollDirectionHorizontal];
     
     UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
     [collection setBackgroundColor:[UIColor clearColor]];
@@ -18,11 +23,27 @@
     [collection setBounces:NO];
     [collection setDataSource:self];
     [collection setDelegate:self];
-    [collection registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:celid];
+    [collection registerClass:[vconfigmodecel class] forCellWithReuseIdentifier:celid];
+    [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
+    self.collection = collection;
     
     [self addSubview:collection];
     
+    NSDictionary *views = @{@"col":collection};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    
     return self;
+}
+
+#pragma mark -
+#pragma mark col del
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
+{
+    return 1;
 }
 
 @end
