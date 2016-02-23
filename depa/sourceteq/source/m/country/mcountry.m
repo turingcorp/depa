79f869dc4df7e1e5c0cfcfc688ad29d336243e03
vauxhall <zero.griffin@gmail.com>
2016-02-23 08:27:12 -0600

    model country

--- depa/sourceteq/source/m/country/mcountry.m
+++ depa/sourceteq/source/m/country/mcountry.m
@@ -1,5 +1,33 @@
 #import "mcountry.h"
 
 @implementation mcountry
+{
+    NSArray *array;
+}
+
+-(instancetype)init
+{
+    self = [super init];
+    
+    array = [NSArray arrayWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"country" withExtension:@"plist"]];
+    
+    return self;
+}
+
+#pragma mark public
+
+-(NSUInteger)count
+{
+    NSUInteger count = array.count;
+    
+    return count;
+}
+
+-(mcountryitem*)item:(NSUInteger)index
+{
+    mcountryitem *item = array[index];
+    
+    return item;
+}
 
 @end
