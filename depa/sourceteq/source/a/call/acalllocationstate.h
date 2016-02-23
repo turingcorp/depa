bd3e83bc33a805e51c2c5ea4f7b815b1a72ee781
vauxhall <zero.griffin@gmail.com>
2016-02-23 14:12:07 -0600

    added api call location state

---
+++ depa/sourceteq/source/a/call/acalllocationstate.h
@@ -0,0 +1,10 @@
+#import "appdel.h"
+#import "acall.h"
+
+@interface acalllocationstate:acall
+
+-(instancetype)init:(mcountryitem*)country;
+
+@property(weak, nonatomic)mcountryitem *country;
+
+@end
---
+++ depa/sourceteq/source/a/call/acalllocationstate.m
@@ -0,0 +1,26 @@
+#import "acalllocationstate.h"
+
+@implementation acalllocationstate
+
+-(instancetype)init:(mcountryitem*)country
+{
+    NSString *vars = [NSString stringWithFormat:@"%@&limit=0&offset=0", variables];
+    
+    self.country = country;
+    self = [super init:vars];
+    self.keyforendpoint = @"search";
+    self.parser = [aparserlocation class];
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark acall
+
+-(void)buildendpoint:(NSDictionary*)params
+{
+    NSString *rawendpoint = params[@"search"];
+    self.endpoint = [NSString stringWithFormat:rawendpoint, self.country.countryid];
+}
+
+@end
