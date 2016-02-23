1b669553327139f950ccb9b04fb92ed992976e76
vauxhall <zero.griffin@gmail.com>
2016-02-23 12:33:04 -0600

    model

--- depa/sourceteq/source/m/configtyp/mconfigtypeapartment.m
+++ depa/sourceteq/source/m/configtyp/mconfigtypeapartment.m
@@ -2,6 +2,21 @@
 
 @implementation mconfigtypeapartment
 
+#pragma mark -
+#pragma mark typ protocol
 
+-(NSString*)title
+{
+    NSString *title = NSLocalizedString(@"", nil);
+    
+    return title;
+}
+
+-(NSString*)assetname
+{
+    NSString *assetname = @"";
+    
+    return assetname;
+}
 
 @end
--- depa/sourceteq/source/m/configtyp/mconfigtyphouse.m
+++ depa/sourceteq/source/m/configtyp/mconfigtyphouse.m
@@ -2,4 +2,21 @@
 
 @implementation mconfigtyphouse
 
+#pragma mark -
+#pragma mark typ protocol
+
+-(NSString*)title
+{
+    NSString *title = NSLocalizedString(@"", nil);
+    
+    return title;
+}
+
+-(NSString*)assetname
+{
+    NSString *assetname = @"";
+    
+    return assetname;
+}
+
 @end
