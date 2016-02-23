c2af03f9922dfdbb0e6ae5253647f5af36777351
vauxhall <zero.griffin@gmail.com>
2016-02-23 12:55:41 -0600

    model

--- depa/sourceteq/source/v/config/vconfigtypecel.h
+++ depa/sourceteq/source/v/config/vconfigtypecel.h
@@ -1,5 +1,11 @@
 #import "appdel.h"
+#import "mconfigtypprotocol.h"
 
 @interface vconfigtypecel:UICollectionViewCell
+
+-(void)config:(id<mconfigtypprotocol>)model;
+
+@property(weak, nonatomic)UILabel *title;
+@property(weak, nonatomic)UIImageView *icon;
 
 @end
--- depa/sourceteq/source/v/config/vconfigtypecel.m
+++ depa/sourceteq/source/v/config/vconfigtypecel.m
@@ -2,4 +2,22 @@
 
 @implementation vconfigtypecel
 
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    
+    
+    
+    return self;
+}
+
+#pragma mark public
+
+-(void)config:(id<mconfigtypprotocol>)model
+{
+    
+}
+
 @end
