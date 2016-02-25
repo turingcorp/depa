c7fb23f8527f276d091a215ede44b40869e91b6e
vauxhall <zero.griffin@gmail.com>
2016-02-25 12:32:39 -0600

    asynchronous display of likes and no

--- depa/sourceteq/source/v/config/vconfiglike.m
+++ depa/sourceteq/source/v/config/vconfiglike.m
@@ -2,4 +2,58 @@
 
 @implementation vconfiglike
 
+-(instancetype)init
+{
+    self = [super init];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    UIColor *color = [UIColor colorWithRed:0.4 green:0.7 blue:0.8 alpha:1];
+    
+    UIImageView *icon = [[UIImageView alloc] init];
+    [icon setClipsToBounds:YES];
+    [icon setUserInteractionEnabled:NO];
+    [icon setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [icon setImage:[[UIImage imageNamed:@"like"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
+    [icon setContentMode:UIViewContentModeScaleAspectFit];
+    [icon setTintColor:color];
+    
+    UILabel *label = [[UILabel alloc] init];
+    [label setBackgroundColor:[UIColor clearColor]];
+    [label setUserInteractionEnabled:NO];
+    [label setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [label setFont:[UIFont fontWithName:fontboldname size:22]];
+    [label setTextColor:color];
+    [label setTextAlignment:NSTextAlignmentRight];
+    
+    [self addSubview:label];
+    [self addSubview:icon];
+    
+    NSDictionary *views = @{@"icon":icon, @"label":label};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[label]-0-[icon(70)]-5-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-5-[icon]-5-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[label]-0-|" options:0 metrics:metrics views:views]];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0),
+                   ^
+                   {
+                       NSString *query = [NSString stringWithFormat:
+                                          @"SELECT COUNT(id) FROM item where status=%@;",
+                                          @(item_status_like)];
+                       NSNumber *value = [db value:query];
+                       NSString *valuestring = [[tools singleton] numbertostring:value];
+                       
+                       dispatch_async(dispatch_get_main_queue(),
+                                      ^
+                                      {
+                                          [label setText:valuestring];
+                                      });
+                   });
+    
+    return self;
+}
+
 @end
