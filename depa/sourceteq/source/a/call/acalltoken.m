5ab12aab7fe281b3f831c2e66e7e906d9ba19607
vauxhall <zero.griffin@gmail.com>
2016-02-28 19:06:24 -0600

    token

--- depa/sourceteq/source/a/call/acalltoken.m
+++ depa/sourceteq/source/a/call/acalltoken.m
@@ -19,7 +19,7 @@
 -(void)buildendpoint:(NSDictionary*)params
 {
     NSString *rawendpoint = params[self.keyforendpoint];
-    self.endpoint = [NSString stringWithFormat:rawendpoint, meliclientid, melisecretkey];
+    self.endpoint = [NSString stringWithFormat:rawendpoint, meliclientid, melisecretkey, melicode];
 }
 
 @end
