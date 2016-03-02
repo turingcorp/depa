acf17143af429291fc74710be2ccd92db2a739f7
vauxhall <zero.griffin@gmail.com>
2016-03-02 09:03:10 -0600

    assets for sort ordering

---
+++ depa/sourceteq/source/m/configsor/mconfigsorrelevance.h
@@ -0,0 +1,13 @@
+//
+//  mconfigsorrelevance.h
+//  depa
+//
+//  Created by zero on 3/2/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mconfigsorrelevance : NSObject
+
+@end
---
+++ depa/sourceteq/source/m/configsor/mconfigsorrelevance.m
@@ -0,0 +1,13 @@
+//
+//  mconfigsorrelevance.m
+//  depa
+//
+//  Created by zero on 3/2/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mconfigsorrelevance.h"
+
+@implementation mconfigsorrelevance
+
+@end
