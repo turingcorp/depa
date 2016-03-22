51d09d9a92865f546d643b50be1734e70635abff
vauxhall <zero.griffin@gmail.com>
2016-03-22 11:13:37 -0600

    config no view

--- depa/sourceteq/source/v/config/vconfigno.h
+++ depa/sourceteq/source/v/config/vconfigno.h
@@ -5,5 +5,7 @@
 
 @property(weak, nonatomic)UILabel *label;
 @property(weak, nonatomic)UIImageView *icon;
+@property(weak, nonatomic)UIView *selector;
+@property(weak, nonatomic)NSLayoutConstraint *layoutmarginleft;
 
 @end
