f7c9c698f626dd501caf8b01e0f60145214f264b
vauxhall <zero.griffin@gmail.com>
2016-03-22 18:35:46 -0600

    share asset

--- depa/sourceteq/source/v/item/vitembar.m
+++ depa/sourceteq/source/v/item/vitembar.m
@@ -20,13 +20,32 @@
     [button.imageView setTintColor:[UIColor colorWithWhite:1 alpha:0.1]];
     [button addTarget:self action:@selector(actionback:) forControlEvents:UIControlEventTouchUpInside];
     
-    [self addSubview:button];
+    UIButton *buttonstar = [[UIButton alloc] init];
+    [buttonstar setImage:[[UIImage imageNamed:@"like"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] forState:UIControlStateNormal];
+    [buttonstar setImage:[[UIImage imageNamed:@"like"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] forState:UIControlStateHighlighted];
+    [buttonstar.imageView setContentMode:UIViewContentModeScaleAspectFit];
+    [buttonstar.imageView setTintColor:[UIColor whiteColor]];
+    [buttonstar setTranslatesAutoresizingMaskIntoConstraints:NO];
     
-    NSDictionary *views = @{@"button":button};
+    UIButton *buttonshare = [[UIButton alloc] init];
+    [buttonshare setImage:[[UIImage imageNamed:@"share"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] forState:UIControlStateNormal];
+    [buttonshare setImage:[[UIImage imageNamed:@"share"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] forState:UIControlStateHighlighted];
+    [buttonshare.imageView setContentMode:UIViewContentModeScaleAspectFit];
+    [buttonshare.imageView setTintColor:[UIColor whiteColor]];
+    [buttonshare setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self addSubview:button];
+    [self addSubview:buttonstar];
+    [self addSubview:buttonshare];
+    
+    NSDictionary *views = @{@"button":button, @"buttonstar":buttonstar, @"buttonshare":buttonshare};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[button(60)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[buttonstar(50)]-0-[buttonshare(50)]-50-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-15-[button(50)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-20-[buttonstar(40)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-20-[buttonshare(40)]" options:0 metrics:metrics views:views]];
     
     return self;
 }
