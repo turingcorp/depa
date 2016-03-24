7d010340f65aacd88bda131a25058dc6cf0b0b4f
vauxhall <zero.griffin@gmail.com>
2016-03-24 12:52:59 -0600

    model baths

---
+++ depa/sourceteq/source/m/item/mitemdetailinfostatsbaths.h
@@ -0,0 +1,13 @@
+//
+//  mitemdetailinfostatsbaths.h
+//  depa
+//
+//  Created by zero on 3/24/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mitemdetailinfostatsbaths : NSObject
+
+@end
---
+++ depa/sourceteq/source/m/item/mitemdetailinfostatsbaths.m
@@ -0,0 +1,13 @@
+//
+//  mitemdetailinfostatsbaths.m
+//  depa
+//
+//  Created by zero on 3/24/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mitemdetailinfostatsbaths.h"
+
+@implementation mitemdetailinfostatsbaths
+
+@end
