20956f0c817841578337afb1f417f3b7e8ffc1a8
vauxhall <zero.griffin@gmail.com>
2016-03-24 15:15:19 -0600

    annotation view

--- depa/sourceteq/source/v/item/vitemlocationannotation.m
+++ depa/sourceteq/source/v/item/vitemlocationannotation.m
@@ -1,13 +1,13 @@
 #import "vitemlocationannotation.h"
 
-static const NSUInteger annsize = 50;
+static CGFloat const annsize = 80;
 
 @implementation vitemlocationannotation
 
 -(instancetype)init
 {
     self = [super initWithFrame:CGRectMake(0, 0, annsize, annsize)];
-    [self setClipsToBounds:YES];
+    [self setClipsToBounds:NO];
     [self setBackgroundColor:[UIColor clearColor]];
     [self setUserInteractionEnabled:NO];
     
@@ -20,11 +20,12 @@ static const NSUInteger annsize = 50;
     
     [self addSubview:image];
     
+    CGFloat halfsize = annsize / 2.0;
     NSDictionary *views = @{@"image":image};
-    NSDictionary *metrics = @{};
+    NSDictionary *metrics = @{@"half":@(halfsize), @"minushalf":@(-halfsize)};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[image]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[image]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(minushalf)-[image]-(half)-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
