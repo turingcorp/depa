a2586f15b5c969d7de4e45aa46dab41cfde29976
vauxhall <zero.griffin@gmail.com>
2016-03-24 22:43:12 -0600

    amanager can't be weak, needs to be __block

--- depa/sourceteq/source/a/call/amanager.m
+++ depa/sourceteq/source/a/call/amanager.m
@@ -88,7 +88,7 @@
         }
         else
         {
-            __weak typeof(self) weakself = self;
+            __block typeof(self) weakself = self;
             
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0),
                            ^
